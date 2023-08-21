b9cb8ab07caf17fc2d81713e5d49d0d8c6456625
Ayush Shrivastava <ayusrivastava2k3@gmail.com>
2023-08-21 15:06:26 +0530

    Final commit

--- src/main/java/org/example/Heart.java
+++ src/main/java/org/example/Heart.java
@@ -1,2 +1,28 @@
-package org.example;public class Heart {
+package org.example;
+
+public class Heart {
+    private String nameOfAnimal;
+    private int noOfHeart;
+
+    public String getNameOfAnimal() {
+        return nameOfAnimal;
+    }
+
+    public void setNameOfAnimal(String nameOfAnimal) {
+        this.nameOfAnimal = nameOfAnimal;
+    }
+
+    public int getNoOfHeart() {
+        return noOfHeart;
+    }
+
+    public void setNoOfHeart(int noOfHeart) {
+        this.noOfHeart = noOfHeart;
+    }
+
+    public void pump()
+    {
+        System.out.println("Heart is pumping......");
+    }
+
 }
--- src/main/java/org/example/Human.java
+++ src/main/java/org/example/Human.java
@@ -1,2 +1,37 @@
-package org.example;public class Human {
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Human {
+    public Human() {
+    }
+
+    // @Autowired
+    @Autowired
+    @Qualifier("humanHeart")
+    private Heart heart;
+
+    public Human(Heart heart) {
+        this.heart = heart;
+    }
+
+
+/*
+    public void setHeart(Heart heart) {
+        this.heart = heart;
+    }
+
+ */
+    public void startPumping()
+    {
+        if (heart!=null) {
+            heart.pump();
+            System.out.println(heart.getNoOfHeart());
+            System.out.println(heart.getNameOfAnimal());
+        }
+        else {
+            System.out.println("yor are dead....");
+        }
+    }
 }
